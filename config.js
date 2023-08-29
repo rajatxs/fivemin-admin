98ff4cd9efa129a11f37407a8973b2aed093ed20
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-08-29 21:16:54 +0530

    setup Algolia api

--- config.js
+++ config.js
@@ -5,3 +5,5 @@ export const DB_URL = String(process.env.FM_ADMIN_DB_URL);
 export const DB_NAME = String(process.env.FM_ADMIN_DB_NAME);
 export const ADMIN_ID = ObjectId.createFromHexString(String(process.env.FM_ADMIN_ID));
 export const CLOUDINARY_CLOUD_NAME = String(process.env.FM_ADMIN_CLD_NAME);
+export const ALGOLIA_APP_ID = String(process.env.FM_ALGOLIA_APP_ID);
+export const ALGOLIA_API_KEY = String(process.env.FM_ALGOLIA_API_KEY);
